fed01e9ef45b914964cf82b7da191517a3938662
Christian Jauvin <cjauvin@gmail.com>
2014-08-25 10:11:18 -0400

    pep8-ification

--- runkeeper_shoes.py
+++ runkeeper_shoes.py
@@ -6,7 +6,10 @@ from matplotlib.patches import Rectangle
 from matplotlib import rc
 from sklearn.linear_model import LinearRegression
 from datetime import datetime
-import re, time, os, cStringIO
+import re
+import time
+import os
+import cStringIO
 import healthgraph
 
 
@@ -16,12 +19,13 @@ app.config.from_pyfile('./config.py')
 
 @app.route('/')
 def index():
-    if session.has_key('rk_access_token'):
+    if 'rk_access_token' in session:
         return redirect(url_for('predict'))
     else:
+        url = '/'.join((app.config['BASEURL'], 'login'))
         rk_auth_mgr = healthgraph.AuthManager(app.config['CLIENT_ID'],
                                               app.config['CLIENT_SECRET'],
-                                              '/'.join((app.config['BASEURL'], 'login',)))
+                                              url)
         rk_auth_uri = rk_auth_mgr.get_login_url()
         rk_button_img = rk_auth_mgr.get_login_button_url('blue', 'black', 300)
     return """
@@ -30,9 +34,9 @@ def index():
         <h1>Welcome to the Runkeeper Shoe Replacement Predictor!</h1>
         <p>
             <a href="%s">
-	        <img src="%s" alt="RunKeeper Login Button">
-	    </a>
-	</p>
+                <img src="%s" alt="RunKeeper Login Button">
+            </a>
+        </p>
     </body>
 </html>
            """ % (rk_auth_uri, rk_button_img)
@@ -42,9 +46,10 @@ def index():
 def login():
     code = request.args['code']
     if code is not None:
+        url = '/'.join((app.config['BASEURL'], 'login',))
         rk_auth_mgr = healthgraph.AuthManager(app.config['CLIENT_ID'],
                                               app.config['CLIENT_SECRET'],
-                                              '/'.join((app.config['BASEURL'], 'login',)))
+                                              url)
         access_token = rk_auth_mgr.get_access_token(code)
         session['rk_access_token'] = access_token
         return redirect(url_for('predict'))
@@ -53,7 +58,7 @@ def login():
 @app.route('/predict', methods=['GET'])
 def predict():
 
-    if not session.has_key('rk_access_token'):
+    if 'rk_access_token' not in session:
         return redirect(url_for('index'))
 
     if 'bought' not in request.args or \
@@ -62,8 +67,10 @@ def predict():
 <html>
     <body>
         <form action="predict" method="get">
-        You bought your running shoes on: <input type="date" name="bought"><br>
-        Only use date after (optional, if not set will use everything): <input type="date" name="start"><br>
+        You bought your running shoes on:
+              <input type="date" name="bought"><br>
+        Only use date after (optional, if not set will use everything):
+              <input type="date" name="start"><br>
         <input type="submit" value="Predict!">
         </form>
     </body>
@@ -83,17 +90,20 @@ def predict():
         return datetime.fromtimestamp(ts + ts0).strftime('%Y-%m-%d')
 
     data_start_date = None
-    if 'start' in request.args and re.match('^\d{4}-\d{2}-\d{2}$', request.args['start']):
+    if 'start' in request.args and \
+       re.match('^\d{4}-\d{2}-\d{2}$', request.args['start']):
         data_start_date = request.args['start']
     buy_date = request.args['bought']
 
     low = 500
     high = 800
 
-    user = healthgraph.User(session=healthgraph.Session(session['rk_access_token']))
+    sess = healthgraph.Session(session['rk_access_token'])
+    user = healthgraph.User(session=sess)
     act_iter = user.get_fitness_activity_iter()
 
-    dates, dists = zip(*[(act['start_time'], act['total_distance']) for act in act_iter if act['type'] == 'Running'])
+    dates, dists = zip(*[(act['start_time'], act['total_distance'])
+                         for act in act_iter if act['type'] == 'Running'])
     df = pd.DataFrame({'dates': dates, 'dists': dists})
 
     if data_start_date:
@@ -110,7 +120,8 @@ def predict():
     ts0 = timestamps[0]
     timestamps -= ts0
 
-    lr = LinearRegression(fit_intercept=False).fit(timestamps.reshape((df.shape[0], 1)), df.cum_dists)
+    lr = LinearRegression(fit_intercept=False)
+    lr.fit(timestamps.reshape((df.shape[0], 1)), df.cum_dists)
 
     latest_x = ((high + 100) + buy_cum_dist) / lr.coef_
     X = np.linspace(timestamps[0], latest_x)
@@ -121,8 +132,10 @@ def predict():
 
     fig = plt.figure(figsize=(12, 7))
 
-    points = plt.scatter(timestamps, df.buy_dists, color='b', label='Running sessions (cumulative distance)')
-    line, = plt.plot(X, Y, linestyle='--', color='r', label='Linear regression')
+    points = plt.scatter(timestamps, df.buy_dists, color='b',
+                         label='Running sessions (cumulative distance)')
+    line, = plt.plot(X, Y, linestyle='--', color='r',
+                     label='Linear regression')
 
     plt.axhspan(low, high, facecolor='y', alpha=0.25)
     plt.axvspan(low_x, high_x, facecolor='y', alpha=0.25)
@@ -149,7 +162,8 @@ def predict():
                ['Shoes bought (on %s)' % buy_date,
                 'Running sessions (cumulative distance)',
                 'Linear regression',
-                'Replacement zone (from %s to %s)' % (from_ts(low_x), from_ts(high_x))],
+                'Replacement zone (from %s to %s)' % (from_ts(low_x),
+                                                      from_ts(high_x))],
                loc='upper right')
 
     sio = cStringIO.StringIO()
@@ -159,15 +173,24 @@ def predict():
 <html>
    <body>
        <h2>Shoe Replacement Prediction for %s</h2>
-       <p><b>Here's how it works</b>: your running data (date and distance for every running session you have tracked)
+       <p><b>Here's how it works</b>: your running data (date and distance for
+             every running session you have tracked)
           is first downloaded from RunKeeper
-          (with the <a href="http://developer.runkeeper.com/healthgraph" target="_blank">Health Graph API</a>).
-          The distances are then transformed into cumulative distances, and adjusted in such a way that the day you bought
-          your shoes is roughly aligned with a distance of 0. These values are then "fitted" (imperfectly) using a
-          <a href="http://cjauvin.blogspot.ca/2013/10/linear-regression-101.html" target="_blank">linear regression</a>,
+          (with the <a href="http://developer.runkeeper.com/healthgraph"
+                       target="_blank">Health Graph API</a>).
+          The distances are then transformed into cumulative distances,
+          and adjusted in such a way that the day you bought
+          your shoes is roughly aligned with a distance of 0. These values are
+          then "fitted" (imperfectly) using a
+        <a href="http://cjauvin.blogspot.ca/2013/10/linear-regression-101.html"
+             target="_blank">linear regression</a>,
           from which the date range where your shoes are predicted to have
-          accumulated 500 to 800 kilometers of usage can be read (note that these values have been unscientifically gathered and averaged
-          from sources like <a href="http://www.runnersworld.com/running-shoes/running-shoe-faq?page=single" target="_blank">this</a>).
+          accumulated 500 to 800 kilometers of usage can be read (note that
+          these values have been unscientifically gathered and averaged
+          from sources like
+<a
+ href="http://www.runnersworld.com/running-shoes/running-shoe-faq?page=single"
+ target="_blank">this</a>).
        </p>
        <img src="data:image/png;base64,%s"/>
    </body>
